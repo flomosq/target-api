76b4d7d275597edb9033e6d7186252907977687a
Florencia Mosquera <florenciamosquera.c@gmail.com>
2021-02-23 16:19:32 -0300

    Sign up user incorrect info test

--- spec/requests/devise_spec.rb
+++ spec/requests/devise_spec.rb
@@ -14,4 +14,18 @@ RSpec.describe 'Devise Token Auth', :type => :request do
     expect(response.content_type).to eq('application/json; charset=utf-8')
     expect(response).to have_http_status(:ok)
   end
+
+  it 'does not register a user if data is incorrect' do
+    headers = { 'ACCEPT' => 'application/json' }
+    params = { 
+        :email => 'florencia.mosquera@rootstrap.com',
+        :password => 'password',
+        :password_confirmation => 'password2',
+        :gender => :female
+    }
+    post '/auth', :params => params, :headers => headers
+
+    expect(response.content_type).to eq('application/json; charset=utf-8')
+    expect(response).to have_http_status(:unprocessable_entity)
+  end
 end
