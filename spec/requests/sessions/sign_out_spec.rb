b7ee714c5e4dc43a4fc1e2fe707e41e30115e491
Florencia Mosquera <florenciamosquera.c@gmail.com>
2021-02-25 18:03:45 -0300

    Change wrong initialization of auth headers

--- spec/requests/sessions/sign_out_spec.rb
+++ spec/requests/sessions/sign_out_spec.rb
@@ -2,7 +2,7 @@ require 'rails_helper'
 
 RSpec.describe 'DELETE api/v1/users/sign_out', type: :request do
   let(:user) { create(:user) }
-  let(:req_headers) { headers }
+  let(:req_headers) { auth_headers }
 
   subject { delete destroy_user_session_path, headers: req_headers }
 
