336a65e6d4038a9dc2bf76f81f4966d4555a4d0f
Florencia Mosquera <florenciamosquera.c@gmail.com>
2021-03-01 15:04:56 -0300

    Create topic model

---
+++ spec/models/topic_spec.rb
@@ -0,0 +1,23 @@
+# == Schema Information
+#
+# Table name: topics
+#
+#  id         :bigint           not null, primary key
+#  name       :string           not null
+#  created_at :datetime         not null
+#  updated_at :datetime         not null
+#
+# Indexes
+#
+#  index_topics_on_name  (name) UNIQUE
+#
+require 'rails_helper'
+
+RSpec.describe Topic, type: :model do
+  subject { build(:topic) }
+
+  describe 'validations' do
+    it { is_expected.to validate_presence_of(:name) }
+    it { is_expected.to validate_uniqueness_of(:name).case_insensitive }
+  end
+end
