7cc5eb533924fd0c7627ebc36a52fbb7f20c092b
Florencia Mosquera <florenciamosquera.c@gmail.com>
2021-03-01 18:37:41 -0300

    Create target endpoint

---
+++ app/controllers/api/v1/targets_controller.rb
@@ -0,0 +1,20 @@
+module Api
+  module V1
+    class TargetsController < ApplicationController
+      def create
+        @target = user_targets.create!(target_params)
+        render json: { target: @target }
+      end
+
+      private
+
+      def user_targets
+        current_user.targets
+      end
+
+      def target_params
+        params.require(:target).permit(:topic_id, :title, :radius, :latitude, :longitude)
+      end
+    end
+  end
+end
