c162a4ac62b65ad8213bade42c6f6c252fd119e4
Florencia Mosquera <florenciamosquera.c@gmail.com>
2021-03-03 15:32:20 -0300

    Delete target returns a successful response

---
+++ spec/requests/api/v1/targets/destroy_spec.rb
@@ -0,0 +1,16 @@
+RSpec.describe 'POST api/v1/targets', type: :request do
+  let(:user) { create(:user) }
+  let!(:target) { create(:target, user: user) }
+
+  let(:headers) { auth_headers }
+
+  subject { delete api_v1_target_path(target.id), headers: headers }
+
+  context 'when the request is correct' do
+    it 'returns a successful response' do
+      subject
+
+      expect(response).to be_successful
+    end
+  end
+end
