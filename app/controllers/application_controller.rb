64217433205d15d59086bcc0aa73c8032e9875c1
Florencia Mosquera <florenciamosquera.c@gmail.com>
2021-03-04 17:24:13 -0300

    Change project for activeadmin

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,12 +1,3 @@
-class ApplicationController < ActionController::API
-  include DeviseTokenAuth::Concerns::SetUserByToken
-
-  before_action :configure_permitted_parameters, if: :devise_controller?
-  before_action :authenticate_user!, unless: :devise_controller?
-
-  protected
-
-  def configure_permitted_parameters
-    devise_parameter_sanitizer.permit(:sign_up, keys: %w[gender])
-  end
+class ApplicationController < ActionController::Base
+  protect_from_forgery with: :exception
 end
