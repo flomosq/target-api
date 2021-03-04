629e0c4908826d5b3eae054ea8dbdd4bf7adf01d
Florencia Mosquera <florenciamosquera.c@gmail.com>
2021-03-04 17:30:19 -0300

    ActiveAdmin and webpacker installation

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,8 @@
 Rails.application.routes.draw do
+  devise_for :admin_users, ActiveAdmin::Devise.config
   mount_devise_token_auth_for 'User', at: 'api/v1/users'
+
+  ActiveAdmin.routes(self)
 
   namespace :api do
     namespace :v1, defaults: { format: :json } do
