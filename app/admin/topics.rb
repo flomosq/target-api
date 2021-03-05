4801807c1e13e99c75c45040f331605f9b5a40d0
Florencia Mosquera <florenciamosquera.c@gmail.com>
2021-03-05 16:54:06 -0300

    Fix linting issues

--- app/admin/topics.rb
+++ app/admin/topics.rb
@@ -1,9 +1,8 @@
 ActiveAdmin.register Topic do
-
   # See permitted parameters documentation:
   # https://github.com/activeadmin/activeadmin/blob/master/docs/2-resource-customization.md#setting-up-strong-parameters
 
   permit_params :name
 
-  filter :name  
+  filter :name
 end
