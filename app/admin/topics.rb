e61840e8e7e0a18589f8682f9b93e977be8021ec
Florencia Mosquera <florenciamosquera.c@gmail.com>
2021-03-05 16:48:58 -0300

    Admin Topics CRUD

---
+++ app/admin/topics.rb
@@ -0,0 +1,9 @@
+ActiveAdmin.register Topic do
+
+  # See permitted parameters documentation:
+  # https://github.com/activeadmin/activeadmin/blob/master/docs/2-resource-customization.md#setting-up-strong-parameters
+
+  permit_params :name
+
+  filter :name  
+end
