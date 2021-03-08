bbdd7f515f5dc7866a795d9df675ed4c6954ffc4
Florencia Mosquera <florenciamosquera.c@gmail.com>
2021-03-08 09:12:49 -0300

    Admin list targets

---
+++ app/admin/targets.rb
@@ -0,0 +1,28 @@
+ActiveAdmin.register Target do
+  actions :index, :show
+
+  permit_params :user_id, :topic_id, :title, :radius, :latitude, :longitude
+
+  index do
+    id_column
+    column :title
+    column :topic
+    column('User') { |t| t.user.email }
+    column :radius
+    column :created_at
+    actions
+  end
+
+  show do
+    attributes_table do
+      row :title
+      row :topic
+      row('User') { |t| t.user.email }
+      row :radius
+      row :latitude
+      row :longitude
+      row :created_at
+      row :updated_at
+    end
+  end
+end
