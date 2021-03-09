fa85a472f8fb67ac7696e1fc360be2d0419a1164
Florencia Mosquera <florenciamosquera.c@gmail.com>
2021-03-09 14:00:15 -0300

    Edit user profile

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -7,5 +7,6 @@ class ApplicationController < ActionController::Base
 
   def configure_permitted_parameters
     devise_parameter_sanitizer.permit(:sign_up, keys: %w[gender])
+    devise_parameter_sanitizer.permit(:account_update, keys: %w[gender name])
   end
 end
