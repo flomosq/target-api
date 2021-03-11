0db4013908213e2d126a6bb39d0ae879d6530b30
Florencia Mosquera <florenciamosquera.c@gmail.com>
2021-03-11 15:15:02 -0300

    Fix linting issues

--- app/models/validators/email_validator.rb
+++ app/models/validators/email_validator.rb
@@ -2,9 +2,9 @@ module Validators
   class EmailValidator < ActiveModel::EachValidator
     EMAIL_REGEX = /\A([^@\s]+)@((?:[-a-z0-9]+\.)+[a-z]{2,})\z/i.freeze
     def validate_each(record, attribute, value)
-      unless value =~ EMAIL_REGEX
-        record.errors.add attribute, (options[:message] || 'is not an email')
-      end
+      return if value =~ EMAIL_REGEX
+
+      record.errors.add attribute, (options[:message] || 'is not an email')
     end
   end
 end
