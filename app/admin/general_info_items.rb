62aa99790efdb5423bf50450a6b42b8d43d68132
Florencia Mosquera <florenciamosquera.c@gmail.com>
2021-03-12 14:10:41 -0300

    General info item decorator - create content preview

--- app/admin/general_info_items.rb
+++ app/admin/general_info_items.rb
@@ -1,11 +1,13 @@
 ActiveAdmin.register GeneralInfoItem do
+  decorate_with GeneralInfoItemDecorator
+
   permit_params :name, :content
 
   index do
     selectable_column
     id_column
     column :name
-    column :content
+    column :content_preview
     actions
   end
 
