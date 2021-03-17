243de95dd31e360a11562dd638940fdd4dba581d
Florencia Mosquera <florenciamosquera.c@gmail.com>
2021-03-17 12:13:10 -0300

    Use find_or_create_by

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,6 +1,7 @@
-GeneralInfoItem.create!(
-  name: 'about',
+GeneralInfoItem.create_with(
   content: Faker::Lorem.paragraph
+).find_or_create_by!(
+  name: 'about'
 )
 
 AdminUser.create!(email: 'admin@example.com', password: 'password', password_confirmation: 'password') if Rails.env.development?
